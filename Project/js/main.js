183958d9077658a384a879a73de8f9bc2f61ba6e
angularall <ertamilselvans@gmail.com>
2016-12-24 00:41:30 +0530

    Csss Changes

--- Project/js/main.js
+++ Project/js/main.js
@@ -12,6 +12,7 @@
 		for(var i=0;i<20;i++){
 				$scope.temp[i]=i;
 		}
+		$scope.alDirectives=loadNgDirectivies();
 		
 	}]);
 	
@@ -19,6 +20,11 @@
 		var pageData={};
 		pageData=[{'image':'one','id':'1','show':true},{'image':'two','id':'2','show':false},{'image':'three','id':'3','show':false},{'image':'four','id':'4','show':false}];
 		return pageData;
+	}
+	function loadNgDirectivies(){
+		var data=[];
+		data=['ng-if','ng-repeat','ng-controller','ng-click','ng-init','ng-bind','ng-model','ng-show','ng-hide','ng-keyup','ng-app','ng-change','ng-class'];
+		return data;
 	}
 })();	
 	
